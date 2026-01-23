da0f4f0dc0b3a356d5e393fa6220684ab63a26a4
SadikMuktadir <sadikmuktadir2.0@gmail.com>
2026-01-23 22:16:16 +0600

    new arrival added

--- app/(commonlayout)/page.tsx
+++ app/(commonlayout)/page.tsx
@@ -2,6 +2,7 @@ import Banner from '@/src/components/home/Banner';
 import BestSelling from '@/src/components/home/BestSelling';
 import BrowseByCategory from '@/src/components/home/BrowseByCategory';
 import FlashSales from '@/src/components/home/FlashSales';
+import NewArrival from '@/src/components/home/NewArrival';
 
 const Home = () => {
   return (
@@ -17,6 +18,9 @@ const Home = () => {
       </div>
       <div className='mt-[150px]'>
         <BestSelling></BestSelling>
+      </div>
+      <div className='mt-[150px]'>
+        <NewArrival></NewArrival>
       </div>
     </div>
   );
---
+++ src/components/home/NewArrival.tsx
@@ -0,0 +1,14 @@
+const NewArrival = () => {
+  return (
+    <div>
+      <div className='mr-20 mb-10'>
+        <p className='text-[36px] font-bold'>New Arrival</p>
+      </div>
+      <div>
+        <p>Comming...</p>
+      </div>
+    </div>
+  );
+};
+
+export default NewArrival;
