7b0f64773d65dd6fc5727e806110fb4433d5a4e4
SadikMuktadir <sadikmuktadir2.0@gmail.com>
2026-02-19 23:00:58 +0600

    details page updated

--- src/components/product/Details.tsx
+++ src/components/product/Details.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
+import { Button } from '@/components/ui/button';
 import { Rating } from '@/components/ui/Rating';
 import { IProduct } from '@/src/types';
 
@@ -12,18 +13,42 @@ const Details = ({ product }: { product: IProduct }) => {
           className='w-[500px] h-[600px] object-contain'
         />
       </div>
-      <div>
-        <div className=''>
+      <div className='flex flex-col justify-center'>
+        <div>
           <p className='text-[24px] font-semibold'>{product?.name}</p>
         </div>
-        <div className='mt-2'>
-          <Rating value={product?.rating}></Rating>
+        <div className='mt-2 flex'>
+          <div className='mr-2'>
+            <Rating value={product?.rating}></Rating>
+          </div>
+          <div className='mr-2'>
+            <p>(150 Reviews)</p>
+          </div>
+          <div>
+            <p className='text-[#00ff66]'>In Stock</p>
+          </div>
         </div>
         <div className='mt-2'>
           <p className='text-[24px]'>${product?.price}</p>
         </div>
-        <div className='mt-2'>
+        <div className='mt-2 mb-8'>
           <p className='text-[14px]'>${product?.name}</p>
+        </div>
+        <hr />
+        <div className='mt-4'>
+          <Button
+            className='cursor-pointer
+    rounded-none
+    bg-[#db4444]
+    text-white
+    border border-transparent
+    hover:bg-white
+    hover:text-black
+    hover:border-[#db4444]
+    transition-colors'
+          >
+            Buy Now
+          </Button>
         </div>
       </div>
     </div>
